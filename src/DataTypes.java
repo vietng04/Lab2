2d80725e046fedece2fefd446627c5741a14250c
Viet Nguyen <viethoang.nguyen@mail.utoronto.ca>
2024-09-16 12:04:31 -0400

    task_1

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
         long s = 0;
