3eeb7db73a67888df0286d975985a85d65eaa242
vietng04 <viethoang.nguyen@mail.utoronto.ca>
2024-11-19 15:59:05 -0500

    update DataTypes

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,14 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class DataTypes {
-    public static long sum(List<Integer> numbers) {
-
-        long s = 0;
-        // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
-            s += x;
-        }
-        return s + 1;
-    }
+    return null;
 }
